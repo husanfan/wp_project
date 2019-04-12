754d779fd2a5213c3dcbcf75f79c3b69888eaa4d
husanfan <870094953@qq.com>
2019-04-12 22:58:55 +0800

    file-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _lodash from 'lodash';
 import './css/index.css';
+import myImgFile from './assets/images/timg.jpg'
 
 function component() {
     var element = document.createElement('div');
@@ -7,6 +8,9 @@ function component() {
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.innerHTML = "Hello World!";
+    const myImg = new Image();
+    myImg.src = myImgFile;
+    element.appendChild(myImg);
 
     return element;
 }
