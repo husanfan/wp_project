dcd77f02f0648344ec00ec5735445bc06d0fc082
husanfan <870094953@qq.com>
2019-05-12 14:44:59 +0800

    抽取第三方依赖

--- src/js/list.js
+++ src/js/list.js
@@ -1,8 +1,7 @@
 export default function list() {
     const listArr = [1, 2, 3];
-    const listStr = listArr.reduce((pre, val, index) => {
+    return listArr.reduce((pre, val, index) => {
         const cur = "<div>" + val + "<div/>";
         return index > 0 ? pre + cur : cur;
-    }, listArr[0])
-    return listStr;
+    }, listArr[0]);
 }
